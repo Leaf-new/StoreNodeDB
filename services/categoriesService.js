6f5ce544a8de39a47dfff18439bc1c5f526414bb
Leaf-new <cesor60@gmail.com>
2023-11-04 23:29:01 -0500

    Estructura de servicios no funcionales para user y caqtegories

---
+++ services/categoriesService.js
@@ -0,0 +1,35 @@
+// todavia no se a instanciado en categoriesRouter
+class CategorieService{
+
+  constructor(){
+    this.categories = [];
+    //this.generate();
+  }
+
+  generate(){
+
+  }
+
+  create(){
+
+  }
+
+  find(){
+    //return this.categories;
+  }
+
+  findOne(id){
+    //return this.categories.find(item => item.id === id);
+  }
+
+  update(){
+
+  }
+
+  delete(){
+
+  }
+
+}
+
+module.exports = CategorieService;
