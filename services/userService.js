7ee272391d03cb5048497d35e5e8f051ef7dc96b
Leaf-new <cesor60@gmail.com>
2023-11-04 23:38:28 -0500

    mejora

---
+++ services/userService.js
@@ -0,0 +1,35 @@
+// todavia no se a instanciado en usersRouter
+class usersService{
+
+  constructor(){
+    this.users = [];
+    //this.generate();
+  }
+
+  generate(){
+
+  }
+
+  create(){
+
+  }
+
+  find(){
+    //return this.users;
+  }
+
+  findOne(id){
+    //return this.users.find(item => item.id === id);
+  }
+
+  update(){
+
+  }
+
+  delete(){
+
+  }
+
+}
+
+module.exports = usersService;
