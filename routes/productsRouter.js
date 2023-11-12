c07523fda82ce0b263d5619927e8e614a79da395
Leaf-new <cesor60@gmail.com>
2023-11-12 11:20:38 -0500

    se agrego async a los servicios de products y control de errores en patch y delete

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,8 +5,8 @@ const ProductService = require('../services/productService')
 const router = express.Router();
 const service = new ProductService();
 
-router.get('/', (req, res)=>{
-  const product = service.find();
+router.get('/', async (req, res)=>{
+  const product =  await service.find();
   res.json(product);
 });
 
@@ -16,29 +16,44 @@ router.get('/filter', (req,res)=>{
 });
 
 
-router.get('/:id', (req, res) =>{
+router.get('/:id', async (req, res) =>{
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 });
 
-router.post('/', (req, res)=> {
+router.post('/', async (req, res)=> {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.patch('/:id', (req, res)=> {         //put y patch en teoria funcionan
-  const{ id } = req.params;                 //de la misma forma pero patch es para informacion parcial
-  const body = req.body;
-  const product = service.update(id, body);
-  res.json(product);
+router.patch('/:id', async (req, res)=> {
+  try {
+    const{ id } = req.params;
+    const body = req.body;
+    const product =  await service.update(id, body);
+    res.json(product);
+
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+
 });
 
-router.delete('/:id', (req, res)=> {
-  const{ id } =req.params;
-  const rta = service.delete(id);
-  res.json(rta);
+router.delete('/:id', async (req, res)=> {
+  try {
+    const{ id } =req.params;
+    const rta = await service.delete(id);
+    res.json(rta);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+
 });
 
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -19,7 +19,7 @@ class ProductService {
     }
   }
 
-  create(data){
+  async create(data){
     const newProduct = {
       id: faker.string.uuid(),
       ...data // spread operation para concatenar los valores recibidos
@@ -29,14 +29,18 @@ class ProductService {
   }
 
   find(){
-    return this.products;
+    return new Promise((resolve, reject) => {
+      setTimeout(()=>{
+        resolve(this.products);
+      }, 5000);
+    })
   }
 
-  findOne(id){
+  async findOne(id){
     return this.products.find(item => item.id === id);
   }
 
-  update(id, changes){
+  async update(id, changes){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){ // si findIndex no encuntra el elemento devuelve un -1
       throw new Error('product not found')
@@ -49,7 +53,7 @@ class ProductService {
     return this.products[index];
   }
 
-  delete(id){
+  async delete(id){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){ // si findIndex no encuntra el elemento devuelve un -1
       throw new Error('product not found')
