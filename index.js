9fcc5bf804f79a5ebe69ce460e236b015ad438d1
Leaf-new <cesor60@gmail.com>
2023-11-12 12:19:07 -0500

    se agrego middleware de control de errores pero solo se usa en la ruta con el servicio findOne de products

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log('my app');
 const express = require('express');
 const routerApi = require('./routes');
 
+const{logErrors, errorHandler}= require('./middlewares/errorHandler')
 
 // Todos los endpoints especificos van antes de los endopoints dinamicos
 // es decir todos los endpoints que usen parametros por ejemplo, van siempre al final
@@ -21,6 +22,11 @@ app.get('/new-endpoint', (req, res)=>{
 })
 
 routerApi(app);
+
+//colocamos nuestros middlewares de error
+//este sera el orden en que se ejecuten uno detras de otro
+app.use(logErrors);
+app.use(errorHandler);
 
 
 app.listen(port, () =>{
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,24 @@
+function logErrors (err, req, res, next){
+  console.log('soy log errors');
+  console.error(err);
+  next(err);                           // es un middleware de tipo error porque mandamos el error
+}
+
+function errorHandler(err, req, res, next){
+  console.log('soy errorhandler');
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+module.exports={logErrors, errorHandler}
+
+//estos middlewares es mejor definirlos despues de crear el routing
+
+
+//https://platzi.com/comentario/3889360/
+// npm install express-async-errors
+//Solo debemos importarlo en el index.js y la función de routing
+//quedará como estaba.
+//require("express-async-errors");
